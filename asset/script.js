2b527b6b39b7b2a77396610c1cdd17b9604a713a
Tewodros Assefa <teddyassef23@gmail.com>
2023-12-18 08:09:46 -0600

    image edited

--- asset/script.js
+++ asset/script.js
@@ -3,13 +3,58 @@
 var apikey_counter ='nozTYwVSNpTtXIsL9SAJtf3k3LY5Y1qrhgWswAZF';
 var foodApi = 'www.themealdb.com/api/json/v1/1/filter.php?a=';
 var selceted = document.getElementById("country");
+var elements = document. getElementsByClassName("image-s");
+// var elements = document.getElementById("1");
+
+
+var countryApi ="https://www.themealdb.com/api/json/v1/1/filter.php?a=American";
+    
+fetch(countryApi)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+  
+  var foods = [];
+  
+ for(let i = 0; i< data.meals.length  ; i++){
+   foods.push(data.meals[i].strMealThumb);
+ }
+ for(let f = 0; f< foods.length; f++){
+   document.getElementById(f).src = foods[f];
+  }
+
+});
+
+
+var fullApiCountery = "https://api.api-ninjas.com/v1/country?name=United State" + "&X-Api-Key=" + apikey_counter;
+fetch(fullApiCountery)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  
+  document.getElementById("city").textContent = data[0].capital;
+  document.getElementById("name").textContent = data[0].name;
+  document.getElementById("Regin").textContent =  data[0].region;
+  document.getElementById("Populetion").textContent =   data[0].population;
+  document.getElementById("Serface").textContent = data[0].surface_area;
+  document.getElementById("Currency").textContent = data[0].currency.name;
+});
+
+
+
+
+
+
 
 
 country.addEventListener("change", (event) =>{
   var countryName = document.getElementById("country");
   var countryvalue = countryName.value;
   var country = countryName.options[countryName.selectedIndex].text;
-  console.log("country value" , countryvalue)
+  // console.log("country value" , countryvalue)
   
   var fullApiCountery = "https://api.api-ninjas.com/v1/country?name=" + country + "&X-Api-Key=" + apikey_counter;
   fetch(fullApiCountery)
@@ -20,17 +65,16 @@ country.addEventListener("change", (event) =>{
     console.log(data);
     
     var cuntry =     data[0].name;
-  console.log("coutery :" + cuntry);
-    var ApiCountery = "https://restcountries.com/v3.1/translation/" + cuntry ;
+    console.log("coutery :" + cuntry);
+    ApiCountery = "https://restcountries.com/v3.1/translation/" + cuntry ;
     fetch(ApiCountery)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log("country :" , data);
+      
     });
-
-
     // var capital =     data[0].capital;
     // var currency =     data[0].currency.name;
     // var name =     data[0].name;
@@ -40,14 +84,14 @@ country.addEventListener("change", (event) =>{
     // var region =     data.region;
     // var population =     data.population;
     // var internet_users =     data.internet_users;
-
-
-document.getElementById("city").textContent = data[0].capital;
-document.getElementById("name").textContent = data[0].name;
-document.getElementById("Regin").textContent =  data[0].region;
-document.getElementById("Populetion").textContent =   data[0].population;
-document.getElementById("Serface").textContent = data[0].surface_area;
-document.getElementById("Currency").textContent = data[0].currency.name;
+    
+    
+    document.getElementById("city").textContent = data[0].capital;
+    document.getElementById("name").textContent = data[0].name;
+    document.getElementById("Regin").textContent =  data[0].region;
+    document.getElementById("Populetion").textContent =   data[0].population;
+    document.getElementById("Serface").textContent = data[0].surface_area;
+    document.getElementById("Currency").textContent = data[0].currency.name;
     
     
     
@@ -59,34 +103,49 @@ document.getElementById("Currency").textContent = data[0].currency.name;
     })
     .then(function (data) {
       console.log(data);
-    
-      var foods = [];
-
-     for(let i = 0; i< data.meals.length  ; i++){
-      foods.push(data.meals[i].strMealThumb);
-     }
-     for(let f = 0; f< foods.length; f++){
-      document.getElementById(f).src = foods[f];
-      }
-
-    })
-
-
-      console.log(" text of selceted 2 :" , text);
-      console.log(" Value of selceted  :" , countrytext);
       
+      var foods = [];
+      
+      for(let i = 0; i< data.meals.length  ; i++){
+      foods.push(data.meals[i].strMealThumb);
+    }
+    for(let f = 0; f< foods.length; f++){
+      document.getElementById(f).src = foods[f];
+    }
+    
+  })
+  
+  
+  console.log(" text of selceted 2 :" , text);
+  console.log(" Value of selceted  :" , countrytext);
+  
 
-
-    });
 
 });
 
-function imageSwepe(imageId){
-let swap = imageId ;
+});
 
-imageId = 0;
-document.getElementById(0).id = swap;
-this.id = 0;
+// function imageSwepe(imageId){
+//   let swap = imageId ;
+  
+//   imageId = 0;
+//   document.getElementById(0).id = swap;
+//   this.id = 0;
+  
+  
+// }
+// elements.addEventListener('click', function(){
+  // let id = this.id;
+  // let sec = this.src;
+  // let msrc = document.getElementById(0).src;
+  // console.log("this is id :" + event.target.id);
+  // console.log("this is main src :" + msrc);
+  // console.log("this is src is :" + sec);
+  // document.getElementById(0).src = sec;
+  // document.getElementById(id).src = msrc;
 
+// })
 
+elements.onclick = function(event){
+  console.log(event.target.id);
 }
